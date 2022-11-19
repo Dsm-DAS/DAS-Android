5b6cd63dde83ac9f08af69bc7d388a33dff39801
yeon <yeon8747@naver.com>
2022-11-19 22:42:44 +0900

    🗂 :: 기타 정보 ui

---
+++ app/src/main/java/ui/activity/RegisterotherinformationActivity.kt
@@ -0,0 +1,21 @@
+package ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import base.BaseActivity
+import com.example.das_android.R
+import com.example.das_android.databinding.ActivityRegisterOtherInformationBinding
+
+class RegisterotherinformationActivity : BaseActivity<ActivityRegisterOtherInformationBinding> (
+    R.layout.activity_register_other_information
+) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
+    }
+
+
+}
