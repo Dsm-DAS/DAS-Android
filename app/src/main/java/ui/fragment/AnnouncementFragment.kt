95b7ffe570be4d31cd43c705b1b0f5b926ca904c
kim euno <eunoh@dsm.hs.kr>
2022-11-21 22:13:33 +0900

    🗂 :: 프래그먼트 생성

---
+++ app/src/main/java/ui/fragment/AnnouncementFragment.kt
@@ -0,0 +1,19 @@
+package ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import base.BaseFragment
+import com.example.das_android.R
+import com.example.das_android.databinding.FragmentAnnouncementBinding
+import com.example.das_android.databinding.FragmentMenuBinding
+
+class AnnouncementFragment : BaseFragment<FragmentAnnouncementBinding>(R.layout.fragment_menu) {
+    fun initView() {
+        binding.apply{
+
+        }
+    }
+}
---
+++ app/src/main/java/ui/fragment/ClubDetailFragment.kt
@@ -0,0 +1,19 @@
+package ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import base.BaseFragment
+import com.example.das_android.R
+import com.example.das_android.databinding.FragmentClubDetailBinding
+import com.example.das_android.databinding.FragmentHomeBinding
+
+class ClubDetailFragment : BaseFragment<FragmentClubDetailBinding>(R.layout.fragment_club_detail) {
+    fun initView() {
+        binding.apply{
+
+        }
+    }
+}
