ed5f6bdad24ddd96819298eb1d28992018a21514
kim euno <eunoh@dsm.hs.kr>
2022-11-21 22:25:50 +0900

    🗂 :: 메인&바텀

---
+++ app/src/main/java/ui/activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import base.BaseActivity
+import com.example.das_android.R
+import com.example.das_android.databinding.ActivityMainBinding
+
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_login){
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
+    }
+
+}
