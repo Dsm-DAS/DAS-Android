fa3b44fa5ff3567fb73412572d03f3d46432795e
rladmsdh <a01051355543@gmail.com>
2022-11-30 12:09:22 +0900

    🗂 :: 글 탭(동아리) 퍼블리싱

---
+++ app/src/main/java/ui/fragment/WriteClubFragment.kt
@@ -0,0 +1,11 @@
+package ui.fragment
+
+import base.BaseFragment
+import com.example.das_android.R
+import com.example.das_android.databinding.FragmentWriteClubBinding
+
+class WriteClubFragment : BaseFragment<FragmentWriteClubBinding>(R.layout.fragment_write_club) {
+    fun initView(){
+        binding.apply {  }
+    }
+}
