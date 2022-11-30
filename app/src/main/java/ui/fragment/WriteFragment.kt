e5e0d5a566374d56bfbc93367f1a17ead4200790
rladmsdh <a01051355543@gmail.com>
2022-11-30 12:08:07 +0900

    🗂 :: 글 탭(모집공고) 퍼블리싱

---
+++ app/src/main/java/ui/fragment/WriteFragment.kt
@@ -0,0 +1,11 @@
+package ui.fragment
+
+import base.BaseFragment
+import com.example.das_android.R
+import com.example.das_android.databinding.FragmentWriteBinding
+
+class WriteFragment : BaseFragment<FragmentWriteBinding>(R.layout.fragment_write) {
+    fun initView(){
+        binding.apply {  }
+    }
+}
