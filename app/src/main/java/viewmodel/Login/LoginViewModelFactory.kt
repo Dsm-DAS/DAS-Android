de93b727c173124d40e124af86ed5108f9eeaaa5
rladmsdh <a01051355543@gmail.com>
2022-12-12 09:25:00 +0900

    🗂 :: 로그인

---
+++ app/src/main/java/viewmodel/Login/LoginViewModelFactory.kt
@@ -0,0 +1,13 @@
+package viewmodel.Login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import data.api.user.login.LoginRepository
+
+@Suppress("UNCHECKED_CAST")
+class LoginViewModelFactory(private val loginRepository: LoginRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return LoginViewModel(loginRepository) as T
+    }
+}
