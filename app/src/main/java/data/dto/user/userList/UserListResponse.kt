e9ca752319c3c8f94d9acb83dcf60d2783284a9e
rladmsdh <a01051355543@gmail.com>
2022-12-14 10:35:02 +0900

    🗂 :: 학생 리사이클러뷰

---
+++ app/src/main/java/data/dto/user/userList/UserListResponse.kt
@@ -0,0 +1,14 @@
+package data.dto.user.userList
+
+import android.media.Image
+import retrofit2.http.Url
+
+data class UserListResponse (
+    val user_id : Int,
+    val name : String,
+    val profile_image_url : String,
+    val viwe_count : Int,
+    val grade : String,
+    val class_num : String,
+    val introduce : String,
+    )
